673aa41d34323bd8689a0170ed5395b320dfccad
Dima Simko <dima70593@ukr.net>
2025-09-21 22:59:27 +0000

    Application deployed

--- file/connection.php
+++ file/connection.php
@@ -1,8 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "bloodbank";
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
 $conn = new mysqli($servername, $username, $password, $dbname);
 if(!$conn){
  die('Could not Connect MySql:' .mysql_error());
